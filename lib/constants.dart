979644319eb1c468631ba1364102cc0b94a58d6a
Gyawu Baffour Kofi <baffourthe3rd@gmail.com>
2023-11-07 12:05:31 +0100

    update project structure,add user_model and model dir, add components , add screens dir , add forgot_password_screen registration_screen splash_screen .dart , add constants

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+const Color? bodyColor = Color(0xFFFFFFFF);
+Color buttonColor = Color(0xFF37B943);
+Color buttonTextColor = Color(0xFFFFFFFF);
+Color formFieldColor = Color(0xFFEEEEEE);
+Color formFieldTitleColor = Color(0xFF858585);
+Color formFieldTextColor = Color(0xFF000000);
+Color textColor = Color(0xFF000000);
+Color emphasisTextColor = Color(0xFF00E0FF);
+ 
