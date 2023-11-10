1c63fd542252f0cbdbc7726444864e35cd86d5e7
Akwasititus <www.ayamgatitus@gmail.com>
2023-11-10 11:33:43 +0100

    color fix

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 
-const Color? bodyColor = Color(0xFFEEEEEE);
+const Color? bodyColor = Color(0xFFFFFFFF);
 const Color buttonColor = Color(0xFF37B943);
 const Color buttonTextColor = Color(0xFFFFFFFF);
-const Color formFieldColor = Color(0xFFFFFFFF);
+const Color formFieldColor = Color(0xFFEEEEEE);
 const Color formFieldHintColor = Color(0xFF858585);
 const Color formFieldTextColor = Color(0xFF000000);
 const Color textColor = Color(0xFF000000);
